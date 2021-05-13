d78e561e09099f38acb6be62d1cdf7ac520ca30a
Y_Moshe <moshe.nehemiah@gmail.com>
2021-05-13 17:12:56 +0300

    Category model

--- models/category.js
+++ models/category.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const { Schema } = mongoose;
 
-const categorySchema =  Schema({
+const categorySchema = new Schema({
   name: {
     type: String,
     required: 'Category name is required!',
