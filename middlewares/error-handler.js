cc4fcf131fd117f5db0eac5e8ce7b3280d9fabbf
Y_Moshe <moshe.nehemiah@gmail.com>
2021-05-25 23:11:32 +0300

    Indent spaces

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -3,43 +3,43 @@
  * And trying to find out which error message to send back to the Response!
  */
 const middleware = (error, req, res, next) => {
-  if (error) {
-      let message = '';
-      let status = error?.status || 500;
+    if (error) {
+        let message = '';
+        let status = error?.status || 500;
 
-      switch (error.name) {
-          // If the error thrown was our CustomError
-          // We know that there's a described error message to send back
-          case 'CustomError':
-              message = error.message;
-              break;
-          case 'TokenExpiredError':
-              message = 'Invalid token or old one.';
-              status = 401;
-              break;
-          case 'ValidationError':
-              /**
-               * ValidationError in mongoose can occurs by a few "Invalid statement"
-               * So what we do here is getting the first 'path', for example 'username' in the schema
-               * Then accessing its error message.
-               */
-              const path = Object.keys(error.errors)[0];
-              message = error.errors[path].message;
-              status = 400;
-              break;
-          case 'CastError':
-              message = 'An invalid Paramater';
-              status = 400;
-              break;
-          default:
-              message = 'An unknown error has occurred';
-      }
+        switch (error.name) {
+            // If the error thrown was our CustomError
+            // We know that there's a described error message to send back
+            case 'CustomError':
+                message = error.message;
+                break;
+            case 'TokenExpiredError':
+                message = 'Invalid token or old one.';
+                status = 401;
+                break;
+            case 'ValidationError':
+                /**
+                 * ValidationError in mongoose can occurs by a few "Invalid statement"
+                 * So what we do here is getting the first 'path', for example 'username' in the schema
+                 * Then accessing its error message.
+                 */
+                const path = Object.keys(error.errors)[0];
+                message = error.errors[path].message;
+                status = 400;
+                break;
+            case 'CastError':
+                message = 'An invalid Paramater';
+                status = 400;
+                break;
+            default:
+                message = 'An unknown error has occurred';
+        }
 
-      console.log(error);
-      res.status(status).json({ message });
-  } else {
-      next();
-  }
+        console.log(error);
+        res.status(status).json({ message });
+    } else {
+        next();
+    }
 };
 
 module.exports = middleware;
--- middlewares/validate-ids.js
+++ middlewares/validate-ids.js
@@ -6,17 +6,17 @@ const { isValidId, CustomError } = require('../utils');
  * And Then, will validate the paramter value with `isValidId` utils!
  */
 const validateIds = async (req, res, next) => {
-  try {
-      const idsEntry = Object.keys(req.params).filter(key => /id/ig.test(key));
-      const test = idsEntry.every(key => isValidId(req.params[key]));
+    try {
+        const idsEntry = Object.keys(req.params).filter(key => /id/ig.test(key));
+        const test = idsEntry.every(key => isValidId(req.params[key]));
 
-      if (!test) {
-          throw new CustomError('Invalid id paramater!', 400);
-      }
-      next();
-  } catch (error) {
-      next( error );
-  }
+        if (!test) {
+            throw new CustomError('Invalid id paramater!', 400);
+        }
+        next();
+    } catch (error) {
+        next( error );
+    }
 };
 
 module.exports = validateIds;
