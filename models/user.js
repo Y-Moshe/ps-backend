2c05f68c9d874c46a613892d5e50ef2251fb945b
Y_Moshe <moshe.nehemiah@gmail.com>
2021-05-25 22:57:13 +0300

    User model

--- models/user.js
+++ models/user.js
@@ -1,61 +1,76 @@
-const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
-const mongoosePaginateV2 = require('mongoose-paginate-v2');
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose'),
+      uniqueValidator = require('mongoose-unique-validator'),
+      mongoosePaginateV2 = require('mongoose-paginate-v2');
+
+const {
+    isValidEmail,
+    isValidLink,
+    isValidUserName
+} = require('./validators');
+const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  firstName: {
-    type: String,
-    required: true
-  },
-  lastName: {
-    type: String,
-    required: true
-  },
-  userName: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  imagePath: {
-    type: String,
-    required: true
-  },
-  creationDate: {
-    type: Number,
-    required: true
-  },
-  isVerified: {
-    type: Boolean,
-    default: false
-  },
-  orders: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Order'
+    email: {
+        type: String,
+        required: 'email address is required!',
+        validate: {
+            validator: isValidEmail,
+            message: 'Invalid email address!'
+        },
+        unique: 'email address is already exists!'
+    },
+    password: {
+        type: String,
+        required: 'password is required!'
+    },
+    firstName: {
+        type: String,
+        minLength: [3, 'first name is too short']
+    },
+    lastName: {
+        type: String,
+        minLength: [3, 'last name is too short'],
+    },
+    userName: {
+        type: String,
+        required: 'user name is required!',
+        validate: {
+            validator: isValidUserName,
+            message: 'Invalid user name!'
+        },
+        unique: 'user name is already exists'
+    },
+    imagePath: {
+        type: String,
+        required: 'User image is required!',
+        validate: {
+            validator: isValidLink,
+            message: 'Invalid image link!'
+        }
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    orders: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Order'
+        }
+    ],
+    role: {
+        type: Schema.Types.ObjectId,
+        required: 'User role must be set!',
+        ref: 'Role'
+    },
+
+    creationDate: {
+        type: Number,
+        default: new Date()
     }
-  ],
-  role: {
-    type: Schema.Types.ObjectId,
-    required: true,
-    ref: 'Role'
-  },
-  isBanned: {
-    type: Boolean,
-    default: false
-  }
 });
 
-userSchema.plugin(uniqueValidator);
-userSchema.plugin(mongoosePaginateV2);
+userSchema.plugin( uniqueValidator );
+userSchema.plugin( mongoosePaginateV2 );
 
 module.exports = mongoose.model('User', userSchema);
--- models/validators/index.js
+++ models/validators/index.js
@@ -1,11 +1,11 @@
 const isValidEmail = value => /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/igm.test(value);
 const isValidPhone = value => /^(\+?\d{5}|\d{3})-?\d{3}-?\d{4}$/.test(value);
 const isValidLink = value => /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/mg.test(value);
-// const isValidUserName = value => /expression/g.test(value);
+const isValidUserName = value => /^[a-zA-z]\w+/g.test(value);
 
 module.exports = {
     isValidEmail,
     isValidPhone,
     isValidLink,
-    // isValidUserName
+    isValidUserName
 }
