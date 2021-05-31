c4417091c8447d58dd22d0c48a867727c02de4b1
Y_Moshe <moshe.nehemiah@gmail.com>
2021-05-31 23:51:21 +0300

    Authenticate middleware issue*fixed

--- passport.js
+++ passport.js
@@ -4,20 +4,20 @@ const passport = require('passport'),
       passportJwt = require('passport-jwt'),
       JwtStrategy = passportJwt.Strategy,
       ExtractJwt = passportJwt.ExtractJwt;
+      
 const { JWT_SECRET } = require('./config');
-
 const { User } = require('./models');
 
 /**
 * After a success authentication on LocalStrategy or JwtStrategy passport saves user data on "req.user"
-* "passport.authentication" or "jwt-auth" from the middlewares folder should be used before!
+* "passport.authenticate" or "authenticate" from the middlewares folder should be used before!
 */
 passport.use(new LocalStrategy({
     usernameField: 'email',
     passwordField: 'password',
     session: false
 }, (username, password, done) => {
-    User.findOne({ email: username })
+    User.findOne({ email: username }).lean()
         .then(user => {
         if (!user) {
             return done(null, false, { message: 'Incorrect email!' });
@@ -36,5 +36,5 @@ passport.use(new JwtStrategy({
     secretOrKey: JWT_SECRET,
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
 }, (payload, done) => {
-    done(null, payload.user);
+    done(null, payload);
 }));
