b75aea9331c0eb7d7b8f7aacbaf3d3d45ec1336e
Y_Moshe <moshe.nehemiah@gmail.com>
2021-06-03 21:59:02 +0300

    Regex fix

--- models/validators/index.js
+++ models/validators/index.js
@@ -1,7 +1,7 @@
 const isValidEmail = value => /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/igm.test(value);
 const isValidPhone = value => /^(\+?\d{5}|\d{3})-?\d{3}-?\d{4}$/.test(value);
 const isValidLink = value => /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/mg.test(value);
-const isValidUserName = value => /^[a-zA-z]\w+/g.test(value);
+const isValidUserName = value => /^[a-zA-z]\w+$/g.test(value);
 
 module.exports = {
     isValidEmail,
