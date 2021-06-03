8114d452644f162e39421fcaed76fab56a72acbb
Y_Moshe <moshe.nehemiah@gmail.com>
2021-06-03 22:04:33 +0300

    Improve users controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,127 +1,208 @@
-const { getPopulateQuery } = require('../utils');
-const { User, Order } = require('../models');
+const { getPopulateQuery, CustomError } = require('../utils');
+const { User, Role } = require('../models');
+const { isValidEmail, isValidUserName } = require('../models/validators');
 
-// GET: /api/v@/users (Admin Route)
-const getUsers = (req, res, next) => {
-  const populateQuery = getPopulateQuery(req.query, 'role', 'orders');
-  const pageSize = +req.query.pageSize;
-  const currentPage = +req.query.currentPage;
+const GET_ALLOWED_PROPS = '_id firstName lastName userName role imagePath creationDate';
 
-  User.paginate({}, {
-      page: currentPage,
-      limit: pageSize,
-      populate: populateQuery
-    }).then(results => {
+// GET: /api/v@/users (Protected)
+const getUsers = async (req, res, next) => {
+    try {
+        const populateQuery = getPopulateQuery(req.query, 'role', 'orders');
+        const { page, per_page } = req.query;
 
-    res.status(200).json( results );
-  }).catch(error => next( error ));
+        const paginateResponse = await User.paginate({}, {
+            page: page || 1,
+            limit: per_page || 10,
+            populate: populateQuery,
+            select: '-password',
+            lean: true,
+            leanWithId: false
+        });
+
+        const status = paginateResponse.totalDocs > 0 ? 200 : 204;
+        res.status( status ).json( paginateResponse );
+    } catch (error) {
+        next( error );
+    }
 };
 
 // GET: /api/v@/users/search
-const searchUser = (req, res, next) => {
-  const populateQuery = getPopulateQuery(req.query, 'role', 'orders');
-  const pageSize = +req.query.pageSize;
-  const currentPage = +req.query.currentPage;
-  const searchQuery = req.query.q;
+const searchUser = async (req, res, next) => {
+    try {
+        const populateQuery = getPopulateQuery(req.query, 'role');
+        const { page, per_page, by, query } = req.query;
 
-  const regEx = new RegExp(searchQuery, 'ig');
+        if ( !by?.trim() || !query?.trim() ) {
+            throw new CustomError('Missing required parameters!', 400);
+        }
 
-  User.paginate({ userName: regEx }, {
-      page: currentPage,
-      limit: pageSize,
-      populate: populateQuery
-    }).then(results => {
+        const regex = new RegExp(query?.trim(), 'ig');
+        const isAllowed = ['firstName', 'lastName', 'userName'].includes(by.trim());
 
-    res.status(200).json( results );
-  }).catch(error => next( error ));
+        if ( !isAllowed ) {
+            throw new CustomError('Invalid search by parameter!', 400);
+        }
+        
+        const paginateResponse = await User.paginate({ [ by ]: regex }, {
+            page: page || 1,
+            limit: per_page || 10,
+            populate: populateQuery,
+            select: GET_ALLOWED_PROPS,
+            lean: true,
+            leanWithId: false
+        });
+
+        const status = paginateResponse.totalDocs > 0 ? 200 : 204;
+        res.status( status ).json( paginateResponse );
+    } catch (error) {
+        next( error );
+    }
 };
 
 // GET: /api/v@/users/:id
-const getUser = (req, res, next) => {
-  const populateQuery = getPopulateQuery(req.query, 'role', 'orders');
-  const userId = req.params.id;
+const getUser = async (req, res, next) => {
+    try {
+        const { id: _id } = req.params;
 
-  User.findOne({ _id: userId }).populate(populateQuery)
-    .then(user => {
-    if (!user) {
-      return res.status(404).json({ message: 'User was not found!' });
+        const user = await User.findById( _id )
+                               .populate( 'role' )
+                               .select( GET_ALLOWED_PROPS )
+                               .lean();
+        if (!user) {
+            throw new CustomError('Could not found the user', 404);
+        }
+        
+        res.status( 200 ).json( user );
+    } catch (error) {
+        next( error );
     }
-
-    res.status(200).json( user );
-  }).catch(error => next( error ));
 };
 
-// GET: /api/v@/users/check/username
-const checkAvailability = (req, res, next) => {
-  const { userName } = req.body;
+// GET: /api/v@/users/availability
+const checkAvailability = async (req, res, next) => {
+    try {
+        const { email, user_name } = req.query;
+        
+        if ( email && user_name ||
+            !email && !user_name ) {
+            throw new CustomError('Bad usage of parameters! email or user_name', 400);
+        }
 
-  User.findOne({ userName }).then(user => {
-    // if user wasn't found send 404 Error
-    if (!user) {
-      return res.status(404).end();
+        if ( email && !isValidEmail( email )) {
+            throw new CustomError('Invalid email address!', 400);
+        }
+
+        if ( user_name && !isValidUserName( user_name )) {
+            throw new CustomError('Invalid user name!', 400);
+        }
+
+        let condition = { };
+        let errMsg = '';
+
+        if ( email ) {
+            condition = { email };
+            errMsg = `There's an account association with ${ email }!`;
+        } else if ( user_name ) {
+            condition = { userName: user_name };
+            errMsg = `The user name ${ user_name } is taken!`;
+        }
+
+        const user = await User.findOne( condition ).lean();
+        const isAvailable = user ? false : true;
+
+        if ( !isAvailable ) {
+            throw new CustomError( errMsg, 401 );
+        }
+        
+        res.status( 200 ).send( isAvailable );
+    } catch (error) {
+        next( error );
     }
-
-    res.status(200).json( user );
-  }).catch(error => next( error ));
 };
 
-// PATCH: /api/v@/users/:id/role
-const updateUser = (req, res, next) => {
-  const _id = req.user._id;
+// PATCH: /api/v@/users/:id (Protected)
+const updateUser = async (req, res, next) => {
+    try {
+        const { id: _id } = req.params;
+        const {
+            firstName,
+            lastName,
+            userName
+        } = req.body;
 
-  const changes = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    userName: req.body.userName
-  };
+        const user = await User.findById( _id ).select( GET_ALLOWED_PROPS );
 
-  User.findOneAndUpdate({ _id },  { ...changes }).then(() => {
-    res.status(200).json({
-      message: 'Changes saved successfully!',
-      ...changes
-    });
-  }).catch(error => next( error ));
-};
+        if ( !user ) {
+            throw new CustomError('Could not found the user', 404);
+        }
 
-// PATCH: /api/v@/users/:id/role
-const changeRole = (req, res, next) => {
-  const { _id, role } = req.body;
+        // If id from the token and req.params are not matchs
+        // Allow only if authenticate user(from the token) has a higher role!
+        if ( _id !== req.user._id && req.userLevel <= user.role ) {
+            throw new CustomError('You are not allowed to do that!', 401);
+        }
 
-  User.updateOne({ _id }, { role })
-    .then(() => res.status(200).end()).catch(error => next( error ));
-};
+        if ( firstName && user.firstName !== firstName ) {
+            user.firstName = firstName;
+        }
 
-// PUT: /api/v@/users/ban
-const banUser = (req, res, next) => {
-  const _id = req.body._id;
+        if ( lastName && user.lastName !== lastName ) {
+            user.lastName = lastName;
+        }
 
-  User.updateOne({ _id }, { isBanned: true })
-    .then(() => res.status(200).end()).catch(error => next( error ));
-};
+        if ( userName && user.userName !== userName ) {
+            user.userName = userName;
+        }
 
-// PUT: /api/v@/users/unban
-const unBanUser = (req, res, next) => {
-  const _id = req.body._id;
+        await user.save();
 
-  User.updateOne({ _id }, { isBanned: false })
-    .then(() => res.status(200).end()).catch(error => next( error ));
-};
-
-// GET: /api/v@/users/:id/orders (Protected)
-const getUserOrders = (req, res, next) => {
-  const populateQuery = getPopulateQuery(req.query, 'products', 'user');
-  const userId = req.params.id;
-
-  Order.find({ user: userId }).populate(populateQuery)
-    .then(orders => {
-    if (orders.length === 0) {
-      return res.status(200).json({
-        message: 'there is no orders yet.'
-      });
+        res.status( 200 ).json({
+            message: `Account of user ${ user.userName } has been updated successfully!`,
+            user
+        });
+    } catch (error) {
+        next( error );
     }
+};
 
-    res.status(200).json( orders );
-  }).catch(error => next( error ));
+// PATCH: /api/v@/users/:id/role (Protected)
+const updateRole = async (req, res, next) => {
+    try {
+        const { id: _id } = req.params;
+        const role = +req.body.role;
+
+        if ( typeof role !== 'number' ) {
+            throw new CustomError('Invalid role level!', 400);
+        }
+
+        const user = await User.findById( _id );
+
+        if ( !user ) {
+            throw new CustomError('Could not found the user', 404);
+        }
+
+        // If id from the token and req.params are not matchs
+        // Allow only if authenticate user(from the token) has a higher role!
+        if ( _id !== req.user._id && req.userLevel <= user.role ) {
+            throw new CustomError('You are not allowed to do that!', 401);
+        }
+
+        // User cannot set other user to role that higher or equal to him!
+        if ( role >= req.userLevel ) {
+            throw new CustomError('You cannot set role that higher than yours or same', 401);
+        }
+
+        user.role = role;
+        await user.save();
+        const roleObj = await Role.findById( role );
+
+        res.status( 200 ).json({
+            message: `The user ${ user.userName } Role updated successfully!`,
+            role: roleObj
+        });
+    } catch (error) {
+        next( error );
+    }
 };
 
 module.exports = {
@@ -130,8 +211,5 @@ module.exports = {
   getUser,
   checkAvailability,
   updateUser,
-  changeRole,
-  banUser,
-  unBanUser,
-  getUserOrders
+  updateRole
 };
