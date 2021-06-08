6d74a14a27d26d80f6eb32b9ce3f88e1ec345562
Y_Moshe <moshe.nehemiah@gmail.com>
2021-06-08 16:46:25 +0300

    mongoose paginate v2

--- models/order.js
+++ models/order.js
@@ -1,73 +1,76 @@
 const mongoose = require('mongoose'),
-      { isValidPhone } = require('./validators');
+      { isValidPhone } = require('./validators'),
+      mongoosePaginateV2 = require('mongoose-paginate-v2');
 const { Schema } = mongoose;
 
 const orderSchema = new Schema({
-  user: {
-    type: Schema.Types.ObjectId,
-    required: 'User id is required in order to bound the order to the user',
-    ref: 'User'
-  },
-
-  country: {
-    type: String,
-    required: 'Country is required!',
-    trim: true
-  },
-  city: {
-    type: String,
-    required: 'City is required!',
-    trim: true
-  },
-  streetAddress: {
-    type: String,
-    trim: true
-  },
-  province: {
-    type: String,
-    trim: true
-  },
-  zipCode: {
-    type: String,
-    required: 'Zip code is required!',
-    trim: true
-  },
-  phone: {
-    type: String,
-    required: 'Phone number is required!',
-    validate: {
-      validator: isValidPhone,
-      message: 'Invalid phone number'
-    }
-  },
-
-  products: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Product'
-    }
-  ],
-  quantities: {
-    [Schema.Types.ObjectId]: Number
-  },
-
-  status: {
-    type: String,
-    enum: {
-      values: ['Processing', 'Canceled', 'Completed'],
-      message: 'Invalid status code'
+    user: {
+        type: Schema.Types.ObjectId,
+        required: 'User id is required in order to bound the order to the user',
+        ref: 'User'
     },
-    default: 'Processing'
-  },
-  receivedAt: {
-    type: Date,
-    default: null
-  },
 
-  creationDate: {
-    type: Date,
-    default: new Date()
-  }
+    country: {
+        type: String,
+        required: 'Country is required!',
+        trim: true
+    },
+    city: {
+        type: String,
+        required: 'City is required!',
+        trim: true
+    },
+    streetAddress: {
+        type: String,
+        trim: true
+    },
+    province: {
+        type: String,
+        trim: true
+    },
+    zipCode: {
+        type: String,
+        required: 'Zip code is required!',
+        trim: true
+    },
+    phone: {
+        type: String,
+        required: 'Phone number is required!',
+        validate: {
+            validator: isValidPhone,
+            message: 'Invalid phone number'
+        }
+    },
+
+    products: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Product'
+        }
+    ],
+    quantities: {
+        [Schema.Types.ObjectId]: Number
+    },
+
+    status: {
+        type: String,
+        enum: {
+            values: ['Processing', 'Canceled', 'Completed'],
+            message: 'Invalid status code'
+        },
+        default: 'Processing'
+    },
+    receivedAt: {
+        type: Date,
+        default: null
+    },
+
+    creationDate: {
+        type: Date,
+        default: new Date()
+    }
 });
+
+orderSchema.plugin( mongoosePaginateV2 );
 
 module.exports = mongoose.model('Order', orderSchema);
--- models/product.js
+++ models/product.js
@@ -1,51 +1,54 @@
 const mongoose = require('mongoose'),
-      { isValidLink } = require('./validators');
+      { isValidLink } = require('./validators'),
+      mongoosePaginateV2 = require('mongoose-paginate-v2');
 const { Schema } = mongoose;
 
 const productSchema = new Schema({
-  name: {
-    type: String,
-    required: 'Product name is required!',
-    minLength: [3, 'Product name too short'],
-    trim: true
-  },
-  description: {
-    type: String,
-    trim: true
-  },
-  imagePath: {
-    type: String,
-    required: 'Product image link is required!',
-    validate: {
-      validator: isValidLink,
-      message: 'Invalid product image link!'
-    }
-  },
-  price: {
-    type: Number,
-    required: 'Product price is required!'
-  },
-  content: {
-    type: String,
-    trim: true
-  },
+    name: {
+        type: String,
+        required: 'Product name is required!',
+        minLength: [3, 'Product name too short'],
+        trim: true
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    imagePath: {
+        type: String,
+        required: 'Product image link is required!',
+        validate: {
+            validator: isValidLink,
+            message: 'Invalid product image link!'
+        }
+    },
+    price: {
+        type: Number,
+        required: 'Product price is required!'
+    },
+    content: {
+        type: String,
+        trim: true
+    },
 
-  category: {
-    type: Schema.Types.ObjectId,
-    required: 'Category id is required in order to bound the product to the relevant category',
-    ref: 'Category'
-  },
-  comments: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Comment'
-    }
-  ],
+    category: {
+        type: Schema.Types.ObjectId,
+        required: 'Category id is required in order to bound the product to the relevant category',
+        ref: 'Category'
+    },
+    comments: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ],
 
-  creationDate: {
-    type: Date,
-    default: new Date()
-  }
+    creationDate: {
+        type: Date,
+        default: new Date()
+    }
 });
+
+productSchema.plugin( mongoosePaginateV2 );
 
 module.exports = mongoose.model('Product', productSchema);
